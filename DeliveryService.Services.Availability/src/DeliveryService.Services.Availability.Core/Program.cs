32f740ca5b4f327f309aca1ae9d70a66cf406002
Mikhail-Z <mike.rzn@yandex.ru>
2020-12-30 20:33:30 +0300

    sln добавлен

---
+++ DeliveryService.Services.Availability/src/DeliveryService.Services.Availability.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeliveryService.Services.Availability.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DeliveryService.Services.Availability/src/DeliveryService.Services.Availability.Infrastructure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DeliveryService.Services.Availability.Infrastructure
+{
+    class TestDto
+    {
+        
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
